994c74d533a0383b651d6d8d461cc6a1b906e1cf
Korlapati Pavan Sai <korlapatipavansai@gmail.com>
2024-04-18 00:33:22 -0500

    Add react app starter configuration

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MapPage from './components/MapPage';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Network Speed Map</h1>
+      <MapPage />
+    </div>
+  );
+}
+
+export default App;
